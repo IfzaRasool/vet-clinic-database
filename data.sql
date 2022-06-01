4cf7007fe0af5afb010d148971f540adfdf3259b
IfzaRasool <ifzarasoolarain@gmail.com>
2022-06-01 19:29:19 +0300

    Add queries

--- data.sql
+++ data.sql
@@ -3,3 +3,23 @@ INSERT INTO animals(id,name,date_of_birth,escape_attempts,neutered,weight_kg) va
 INSERT INTO animals(id,name,date_of_birth,escape_attempts,neutered,weight_kg) values(2,'Gabumon','2018-11-15',2,true,8),(3,'Pikachu','2021-01-07',1,false,15.04),(4,'Devimon','2017-05-12',5,true,11);
 
 INSERT INTO animals(id,name,date_of_birth,escape_attempts,neutered,weight_kg) values(5,'Charmander','2020-02-08',0,false,-11),(6,'Plantmon','2021-11-15',2,true,-5.7),(7,'Squirtle','1993-04-02',3,false,-12.13),(8,'Angemon','2005-06-12',1,true,-45),(9,'Boarmon','2005-06-07',7,true,20.4),(10,'Blossom','1998-10-13',3,true,17),(11,'Ditto','2022-05-14',4,true,22);
+-- Insert the following data into the owners table:
+INSERT INTO owners(full_name,age) values('Sam Smith',34),('Jennifer Orwell',19),('Bob',34),('Melody Pond',77),('Dean Winchester',14),('Jodie Whittarker',38);
+-- Insert the following data into the species table:
+INSERT INTO species(name) values('Pokemon'),('Digimon');
+-- Modify your inserted animals so it includes the species_id value:
+-- If the name ends in "mon" it will be Digimon
+UPDATE ANIMALS SET species_id=2 where name LIKE '%mon';
+-- All other animals are Pokemon
+UPDATE ANIMALS SET species_id=1 where name NOT LIKE '%mon';
+-- Modify your inserted animals to include owner information (owner_id):
+-- Sam Smith owns Agumon.
+ UPDATE ANIMALS SET owner_id=1 where name='Agumon';
+-- Jennifer Orwell owns Gabumon and Pikachu.
+UPDATE ANIMALS SET owner_id=2 where name='Gabumon' AND name='Pikachu';
+-- Melody Pond owns Charmander, Squirtle, and Blossom.
+ UPDATE ANIMALS SET owner_id=4 where name='Charmander' OR name='Squirtle' OR name='Blossom';
+-- Bob owns Devimon and Plantmon.
+ UPDATE ANIMALS SET owner_id=3 where name='Devimon' OR name='Plantmon';
+-- Dean Winchester owns Angemon and Boarmon.
+UPDATE ANIMALS SET owner_id=5 where name='Angemon' OR name='Boarmon';
--- schema.sql
+++ schema.sql
@@ -4,3 +4,15 @@ CREATE DATABASE vet_clinic ;
 CREATE TABLE animals(id INT PRIMARY KEY NOT NULL,name TEXT NOT NULL,date_of_birth date,escape_attempts integer,neutered boolean,weight_kg decimal);
 -- Add Column species.
 ALTER TABLE animals ADD species TEXT;
+--  Create owners table
+CREATE TABLE owners(id SERIAL PRIMARY KEY, full_name Text, age int);
+--  id is set as autoincremented PRIMARY KEY
+ALTER TABLE animals ADD id SERIAL PRIMARY KEY;
+--  Create species table
+CREATE TABLE species(id SERIAL PRIMARY KEY, name Text);
+-- Delete the column;
+ALTER TABLE animals DROP COLUMN species;
+-- Add column species_id which is a foreign key referencing species table
+ALTER TABLE animals ADD COLUMN species_id BIGINT REFERENCES species (id);
+-- Add column owner_id which is a foreign key referencing the owners table
+ALTER TABLE animals ADD COLUMN owner_id BIGINT REFERENCES owners(id);
